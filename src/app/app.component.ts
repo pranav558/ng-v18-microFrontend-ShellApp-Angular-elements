4fc8f770e7bbf783dd220ec218046b6dc38a8499
Pranav Mhapsekar <pranav558@gmail.com>
2025-02-23 04:05:27 +0530

    MFE shellApp code completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { fromEvent } from 'rxjs';
 
+interface Mfe1Details {
+  name: string;
+  email: string;
+}
+interface Mfe2Data {
+  msg: string;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ng-v18-microFrontend-ShellApp-angular-elements';
+  mfe1Details = toSignal(fromEvent<CustomEvent<Mfe1Details>>(window, 'mfe1:reactiveFormData'), {
+    initialValue: new CustomEvent<Mfe1Details>('mfe1:reactiveFormData', {
+      detail: { name: '', email: '' }
+    })
+  });
+  mfe2Details = toSignal(fromEvent<CustomEvent<Mfe2Data>>(window, 'mfe2:data'), {
+    initialValue: new CustomEvent<Mfe2Data>('mfe2:data', { detail : { msg : '' }})
+  });
 }
